544bf30f6aab7b31814064e897b3490e07e10070
LefdRida <74991054+LefdRida@users.noreply.github.com>
2025-12-11 22:02:29 +0000

    Deploying to gh-pages from @ LefdRida/lefdRida.github.io@949893a14217e12d0dd3c987f29185fa081b5c78 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,18 +37,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "post-",
-        
-          title: "",
-        
-        description: "",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/posts/2025/2025-01-20-LLM_from_scratch/";
-          
-        },
-      },{id: "post-hello-world",
+        },{id: "post-hello-world",
         
           title: "Hello World",
         
@@ -90,6 +79,17 @@ ninja.data = [{
         handler: () => {
           
             window.location.href = "/posts/2025/RAG_Blog/";
+          
+        },
+      },{id: "post-llm-from-scratch",
+        
+          title: "LLM From Scratch",
+        
+        description: "",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/posts/2025/LLM_from_scratch/";
           
         },
       },{id: "post-energy-forecasting-with-lstm",
